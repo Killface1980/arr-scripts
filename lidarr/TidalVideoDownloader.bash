40696e93fac0fde5369017d597eaf611fdc0519c
Killface1980 <tmgaertner@googlemail.com>
2023-09-23 17:38:28 +0200

    life is live

--- lidarr/TidalVideoDownloader.bash
+++ lidarr/TidalVideoDownloader.bash
@@ -274,9 +274,6 @@ VideoProcess() {
 				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: 4K Upgrade Found - UNWANTED!"
 				continue
 				# videoType="-video"
-			elif echo "$videoTitle" | grep -i "\(.*live.*\)" | read; then
-				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: Live Video Found - UNWANTED!"
-				continue
 			elif echo "$videoTitle" | grep -i "official" | grep -i "video" | read; then
 				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: Official Music Video Match Found!"
 				videoType="-video"
@@ -285,6 +282,10 @@ VideoProcess() {
 			elif echo "$videoTitle" | grep -i "\(.*unplugged.*\)" | read; then
 				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: Unplugged Video Found!"
 				videoType="-live"
+			elif echo "$videoTitle" | grep -i "\blive\b" | read; then
+			#elif echo "$videoTitle" | grep -i "\(.*live.*\)" | read; then
+				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: Live Video Found - UNWANTED!"
+				continue
 			elif echo $lidarrArtistTrackData | grep -i "$videoTitle" | read; then
 				log "$processCount/$lidarrArtistCount :: $lidarrArtistName :: $tidalVideoProcessNumber/$tidalVideoIdsCount :: $videoTitle ($id) :: Music Video Track Name Match Found!"
 				videoType="-video"
